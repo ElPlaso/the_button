1e6035c2b79061b9dcc92e87068e4ebb13a27e3c
Moo Plah Soe Kusay <plahsoekusay@gmail.com>
2025-03-23 22:10:16 +1300

    allow updating current hand

--- app/src/main/java/com/example/button/MainActivity.kt
+++ app/src/main/java/com/example/button/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.button
 
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,14 +9,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -182,9 +191,7 @@ internal fun generateRandomHand(): MutableList<Pair<String, String>> {
 }
 
 @Composable
-fun Game(modifier: Modifier = Modifier) {
-    val randomHand = generateRandomHand()
-
+fun Hand(hand: MutableList<Pair<String, String>>, modifier: Modifier = Modifier) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(10.dp),
@@ -192,14 +199,32 @@ fun Game(modifier: Modifier = Modifier) {
             .fillMaxWidth()
             .padding(paddingValues = PaddingValues(horizontal = 10.dp))
     ) {
-        randomHand.forEach({
+        hand.forEach {
             val (suit, rank) = it
             CardImage(
                 suit, rank, modifier
                     .fillMaxWidth()
                     .weight(1f)
             )
-        })
+        }
+    }
+}
+
+@SuppressLint("MutableCollectionMutableState")
+@Composable
+fun Game(modifier: Modifier = Modifier) {
+    var hand by remember { mutableStateOf(generateRandomHand()) }
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Hand(hand)
+        Spacer(modifier = Modifier.height(50.dp))
+        Button(onClick = { hand = generateRandomHand() }) {
+            Text("Next")
+        }
     }
 }
 
